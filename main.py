1b4b2e275a7c39329872526b2de9767aa0a270f4
python <python290821@gmail.com>
2021-12-19 19:14:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ def calc(_func):
     y = int(input('enter number: '))
     print(_func(x, y))
 
+def mod_list(l1, _func):
+    for i in range(len(l1)):
+        l1[i] = _func(l1[i])
 
 f2( f1 )
 f2( lambda : print('hello') )
@@ -19,3 +22,6 @@ calc( lambda x,y: x * y)
 # run on the list, for each item invoke the function and put the new value there
 # you can use a function of power 2
 #_list[i] = _func( _list[i] )
+l1 = [1,2,3]
+mod_list(l1, lambda x: x**2)
+print(l1)
