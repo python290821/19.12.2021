073b8bd97c399a94b04d1c666fd144395e32843a
python <python290821@gmail.com>
2021-12-19 18:53:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,13 @@ def calc(_func):
     y = int(input('enter number: '))
     print(_func(x, y))
 
+
 f2( f1 )
 f2( lambda : print('hello') )
 #calc( lambda x,y: x + y)
 calc( lambda x,y: x * y)
+
+# write a function which gets a list and a func as argument
+# run on the list, for each item invoke the function and put the new value there
+# you can use a function of power 2
+#_list[i] = _func( _list[i] )
